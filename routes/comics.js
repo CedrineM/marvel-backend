1dc80a5f545a9b5674ddb5d92d72cba1ca1a5b16
CedrineM <cedrinemilesi@gmail.com>
2025-03-19 17:00:27 +0100

    feat: ajout de plusieurs routes GET dans comics
    - Route pour récupérer les comics liés à un ID personnage
    - Route pour récupérer un comics avec sont ID

--- routes/comics.js
+++ routes/comics.js
@@ -35,5 +35,49 @@ router.get("/comics", async (req, res) => {
   }
 });
 
+//route de récupération d'un comics avec sont ID
+router.get("/comic/:comicId", async (req, res) => {
+  try {
+    const { comicId } = req.params;
+    if (!comicId) {
+      throw {
+        message: "an ID must be given",
+        status: 400,
+      };
+    }
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comic/${comicId}?apiKey=${process.env.MARVEL_API_SECRET}`
+    );
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.error;
+    return res
+      .status(error.status || 500)
+      .json({ message: error.message || "Internal server Error" });
+  }
+});
+
+//route de récupération des comics lier à un ID personnages
+router.get("/comics/:characterId", async (req, res) => {
+  try {
+    const { characterId } = req.params;
+    if (!characterId) {
+      throw {
+        message: "an ID must be given",
+        status: 400,
+      };
+    }
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${process.env.MARVEL_API_SECRET}`
+    );
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.error;
+    return res
+      .status(error.status || 500)
+      .json({ message: error.message || "Internal server Error" });
+  }
+});
+
 //export des routes
 module.exports = router;
